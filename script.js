7a98941359a8d3c57503a6b777182251834cfbd3
Jared Mandap <jaredmandap@gmail.com>
2023-03-16 01:28:43 -0500

    add select term logic + styles. Add math department

---
+++ script.js
@@ -0,0 +1,7 @@
+function selectTerm() {
+  var pageSelect = document.getElementById("pageSelect");
+  var selectedPage = pageSelect.options[pageSelect.selectedIndex].value;
+  if (selectedPage != "") {
+    window.location.href = selectedPage;
+  }
+}
