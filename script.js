ce8fe5c738d2187f2e8d2662cd020c065708e922
Jared Mandap <jaredmandap@gmail.com>
2023-03-16 16:37:09 -0500

    fix directory in main navbar for index, register, personal info

--- script.js
+++
@@ -1,7 +0,0 @@
-function selectTerm() {
-  var pageSelect = document.getElementById("pageSelect");
-  var selectedPage = pageSelect.options[pageSelect.selectedIndex].value;
-  if (selectedPage != "") {
-    window.location.href = selectedPage;
-  }
-}
